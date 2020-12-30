9cd0d380e7864b16934695e21552ebcf7c804c6b
GeopJr <geopjr@geopjr.xyz>
2020-12-30 02:01:25 +0200

    Initial stracture

---
+++ layouts/default.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-spacer />
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <transition name="fade" appear>
+      <v-main>
+        <v-container fill-height>
+          <nuxt />
+        </v-container>
+      </v-main>
+    </transition>
+    <v-footer app absolute rounded padless>
+      <v-card flat tile class="text-center c-footer">
+        <v-card-text class="pa-3">
+          <strong class="accent--text">&copy;</strong> {{ new Date().getFullYear() }} - <strong><a :href="me" style="text-decoration: none;">GeopJr</a></strong> with <v-icon class="accent--text" medium>
+            mdi-heart
+          </v-icon>,
+          <v-icon class="accent--text" medium>
+            mdi-vuejs
+          </v-icon> and <v-icon class="accent--text" medium>
+            mdi-vuetify
+          </v-icon>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import Routes from '@/assets/routes.json'
+
+export default {
+  data () {
+    return {
+      drawer: false,
+      me: 'https://geopjr.xyz/',
+      title: 'Argyle',
+      items: Routes
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.c-footer {
+  width: 100%;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-enter {
+  opacity: 0;
+}
+.v-card__title {
+  word-break: break-word !important;
+}
+html {
+  overflow: auto !important;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,75 @@
+export default {
+  ssr: false,
+  loadingIndicator: {
+    name: 'folding-cube',
+    color: '#7f77bc',
+    background: '#121212'
+  },
+  loading: {
+    color: '#7f77bc'
+  },
+  target: 'static',
+  head: {
+    titleTemplate: '%s - Argyle',
+    title: 'Home',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Just some useful tools' },
+      { name: 'theme-color', content: '#7f77bc' },
+      // OpenGraph
+      { name: 'og:site_name', content: 'GeopJr#4066' },
+      { name: 'og:description', content: 'Just some useful tools' },
+      { name: 'og:title', content: 'Argyle' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:url', content: 'https://geopjr.xyz/' },
+      { name: 'og:image:type', content: 'image/png' },
+      { name: 'og:image', content: 'https://i.imgur.com/H7GmGzQ.png' },
+      // Apple
+      { name: 'apple-mobile-web-app-title', content: 'Argyle' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' }
+    ],
+    link: [
+      { rel: 'canonical', href: 'https://argyle.geopjr.xyz' },
+      // Favicons
+      { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/favicons/favicon-32x32.png?v=2' },
+      { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/favicons/favicon-16x16.png?v=2' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico?v=2' },
+      { rel: 'manifest', href: '/favicons/manifest.json' },
+      { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico', color: '#ffff00' },
+      // Apple
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+      // MEMEME
+      { rel: 'me', href: 'https://tech.lgbt/@GeopJr' },
+      { rel: 'me', href: 'https://github.com/GeopJr' }
+    ]
+  },
+  components: true,
+  // Reduces vuetify size apparently
+  build: {
+    extractCSS: true
+  },
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    ['@nuxtjs/vuetify', { treeShake: true }]
+  ],
+  generate: { fallback: '404.html' },
+  vuetify: {
+    theme: {
+      dark: true,
+      themes: {
+        light: {
+          primary: '#a56262',
+          accent: '#a56262'
+        },
+        dark: {
+          primary: '#7f77bc',
+          accent: '#7f77bc'
+        }
+      }
+    }
+  },
+  css: ['@/assets/transitions.css']
+}
