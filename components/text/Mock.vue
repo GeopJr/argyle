6354370b3c4ff67965f3d3eacb2473ad5e7fb37f
GeopJr <geopjr@geopjr.xyz>
2020-12-30 02:04:59 +0200

    feat(text): mock tool

---
+++ components/text/Mock.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-card>
+    <v-toolbar
+      color="primary"
+    >
+      <v-btn
+        icon
+        dark
+        @click="tool.dialog = false"
+      >
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ tool.name }} - {{ tool.desc }}</v-toolbar-title>
+    </v-toolbar>
+    <v-container fill-height fluid>
+      <v-spacer />
+      <v-col
+        cols="12"
+        md="6"
+        sm="12"
+        align="center"
+      >
+        <v-col
+          cols="12"
+        >
+          <v-textarea
+            v-model="input"
+            filled
+            label="Input"
+            auto-grow
+            rows="10"
+            hide-details
+            @input="inputMock = input.split('').map(x => Math.floor(Math.random() * Math.floor(2)) === 1 ? x.toLowerCase() : x.toUpperCase()).join('')"
+          />
+        </v-col>
+        <v-divider class="my-4" />
+        <v-col
+          cols="12"
+        >
+          <v-textarea
+            v-model="inputMock"
+            filled
+            label="Output"
+            auto-grow
+            rows="10"
+            readonly
+            hide-details
+          />
+        </v-col>
+      </v-col>
+      <v-spacer />
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    tool: {
+      type: Object,
+      default: null
+    }
+  },
+  data: () => ({
+    input: '',
+    inputMock: ''
+  })
+}
+</script>
