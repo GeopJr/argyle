ce07dec02954cc936b26466c7729acc957001a09
GeopJr <geopjr@geopjr.xyz>
2020-12-30 02:05:11 +0200

    feat(text): reverse tool

---
+++ components/text/Reverse.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-card>
+    <v-toolbar
+      color="primary"
+    >
+      <v-btn
+        icon
+        dark
+        @click="tool.dialog = false"
+      >
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ tool.name }} - {{ tool.desc }}</v-toolbar-title>
+    </v-toolbar>
+    <v-container fill-height fluid>
+      <v-spacer />
+      <v-col
+        cols="12"
+        md="6"
+        sm="12"
+        align="center"
+      >
+        <v-col
+          cols="12"
+        >
+          <v-textarea
+            v-model="input"
+            filled
+            label="Input"
+            auto-grow
+            rows="10"
+            hide-details
+            @input="inputRev = input.split(' ').map(x => x.split('').reverse().join('')).reverse().join(' ')"
+          />
+        </v-col>
+        <v-divider class="my-4" />
+        <v-col
+          cols="12"
+        >
+          <v-textarea
+            v-model="inputRev"
+            filled
+            label="Output"
+            auto-grow
+            rows="10"
+            readonly
+            hide-details
+          />
+        </v-col>
+      </v-col>
+      <v-spacer />
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    tool: {
+      type: Object,
+      default: null
+    }
+  },
+  data: () => ({
+    input: '',
+    inputRev: ''
+  })
+}
+</script>
