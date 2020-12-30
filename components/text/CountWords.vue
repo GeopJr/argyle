de7920c095bfaf9bd605eeb78e8dd5929922116e
GeopJr <geopjr@geopjr.xyz>
2020-12-30 02:04:17 +0200

    feat(text): word counter tool

---
+++ components/text/CountWords.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-card>
+    <v-toolbar
+      color="primary"
+    >
+      <v-btn
+        icon
+        dark
+        @click="tool.dialog = false"
+      >
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ tool.name }} - {{ tool.desc }}</v-toolbar-title>
+    </v-toolbar>
+    <v-container fill-height fluid>
+      <v-spacer />
+      <v-col
+        cols="12"
+        md="6"
+        sm="12"
+        align="center"
+      >
+        <v-col
+          cols="12"
+        >
+          <v-textarea
+            v-model="input"
+            filled
+            label="Input"
+            auto-grow
+            rows="10"
+            hide-details
+            @input="counter = input.split(' ').filter(x => x !== null && x.length !== 0).length"
+          />
+        </v-col>
+        <v-divider class="my-4" />
+        <v-col
+          cols="12"
+        >
+          <v-text-field
+            v-model="counter"
+            filled
+            label="Output"
+            readonly
+            hide-details
+          />
+        </v-col>
+      </v-col>
+      <v-spacer />
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    tool: {
+      type: Object,
+      default: null
+    }
+  },
+  data: () => ({
+    input: '',
+    counter: 0
+  })
+}
+</script>
