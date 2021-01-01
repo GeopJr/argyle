7b76370c4110980a49ca6ca056e0b05b0bf08436
GeopJr <geopjr@geopjr.xyz>
2021-01-01 05:26:48 +0200

    feat: PWA

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,23 +8,32 @@ export default {
   loading: {
     color: '#7f77bc'
   },
+  render: {
+    static: {
+      maxAge: 60 * 60 * 24 * 30 * 1000
+    }
+  },
   target: 'static',
   head: {
+    noscript: [{ innerHTML: 'This website requires JavaScript.' }],
+    htmlAttrs: {
+      lang: 'en'
+    },
     titleTemplate: '%s - Argyle',
     title: 'Home',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Just some useful tools' },
+      { hid: 'description', name: 'description', content: 'An offline collection of online tools' },
       { name: 'theme-color', content: '#7f77bc' },
       // OpenGraph
       { name: 'og:site_name', content: 'GeopJr#4066' },
-      { name: 'og:description', content: 'Just some useful tools' },
+      { name: 'og:description', content: 'An offline collection of online tools' },
       { name: 'og:title', content: 'Argyle' },
       { name: 'og:type', content: 'website' },
-      { name: 'og:url', content: 'https://geopjr.xyz/' },
+      { name: 'og:url', content: 'https://argyle.geopjr.xyz/' },
       { name: 'og:image:type', content: 'image/png' },
-      { name: 'og:image', content: 'https://i.imgur.com/H7GmGzQ.png' },
+      { name: 'og:image', content: 'https://i.imgur.com/QKFC8cW.png' },
       // Apple
       { name: 'apple-mobile-web-app-title', content: 'Argyle' },
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
@@ -36,7 +45,6 @@ export default {
       { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/favicons/favicon-32x32.png?v=2' },
       { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/favicons/favicon-16x16.png?v=2' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico?v=2' },
-      { rel: 'manifest', href: '/favicons/manifest.json' },
       { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico', color: '#ffff00' },
       // Apple
@@ -53,9 +61,25 @@ export default {
   },
   buildModules: [
     '@nuxtjs/eslint-module',
+    '@nuxtjs/pwa',
     ['@nuxtjs/vuetify', { customVariables: ['~/assets/variables.scss'], treeShake: true }]
   ],
   generate: { fallback: '404.html' },
+  pwa: {
+    meta: {
+      appleStatusBarStyle: 'black-translucent',
+      theme_color: '#7f77bc',
+      ogSiteName: 'GeopJr#4066',
+      ogTitle: 'Argyle',
+      ogDescription: 'An offline collection of online tools',
+      ogImage: 'https://i.imgur.com/QKFC8cW.png',
+      ogUrl: 'https://argyle.geopjr.xyz'
+
+    },
+    manifest: {
+      background_color: '#121212'
+    }
+  },
   vuetify: {
     breakpoint: {
       thresholds: {
